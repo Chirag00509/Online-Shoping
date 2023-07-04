b307b13444f4169ca523f3842adcb9bfbcde4b62
Chirag00509 <chiraglabha31@gmail.com>
2023-07-04 10:08:19 +0530

    new

---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../../services/product.service';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+
+  productList: any[] = [];
+
+  constructor(private productService : ProductService) {}
+
+  ngOnInit(): void {
+    debugger;
+    this.loadProduct();
+  }
+
+  loadProduct() {
+    debugger
+    this.productService.getAllProducts().subscribe((result) => {
+      this.productList = result
+    })
+  }
+
+}
