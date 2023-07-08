f375757dd1273f4a14aad7c3a1b4d10a95e0031c
Chirag00509 <chiraglabha31@gmail.com>
2023-07-08 16:47:22 +0530

    add reactive form in login and register

--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -1,21 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { UserService } from '../../services/user.service';
+import { FormGroup, FormBuilder, FormControl, Validators, AbstractControl } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
 
-  constructor(private router : Router, private userService : UserService) {}
+  loginForm! : FormGroup
+
+  constructor(private router : Router, private userService : UserService, private formBuilder : FormBuilder) {}
+
+  ngOnInit(): void {
+    this.initializeForm();
+  }
+
+  initializeForm() {
+    this.loginForm = this.formBuilder.group({
+      email : new FormControl('', [ Validators.required, Validators.email ]),
+      password: new FormControl('', [ Validators.required, Validators.minLength(6) ])
+    })
+  }
+
+  getControl(name: any) : AbstractControl | null {
+    return this.loginForm.get(name);
+  }
+
 
   navigatePage() {
     this.router.navigateByUrl('/user/registration')
   }
 
   login(data: any) {
+    console.log(data);
+
     this.userService.getUser().subscribe((users: any[]) => {
       const user = users.find(u => u.email === data.email && u.password === data.password);
       if(user) {
--- src/app/user/registration/registration.component.ts
+++ src/app/user/registration/registration.component.ts
@@ -1,16 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { UserService } from '../../services/user.service';
 import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
+import { AbstractControl, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-registration',
   templateUrl: './registration.component.html',
   styleUrls: ['./registration.component.css']
 })
-export class RegistrationComponent {
+export class RegistrationComponent implements OnInit {
 
-  constructor(private userService : UserService, private router : Router) {}
+  register!: FormGroup
+
+  constructor(private userService : UserService, private router : Router, private formBuilder : FormBuilder) {}
+
+  ngOnInit(): void {
+    this.initializeForm()
+  }
+
+  initializeForm() {
+    this.register = this.formBuilder.group({
+      name : new FormControl('', [ Validators.required ]),
+      email : new FormControl('', [ Validators.required, Validators.email ]),
+      password: new FormControl('', [ Validators.required, Validators.minLength(6) ])
+    })
+  }
+
+  getControl(name: any) : AbstractControl | null {
+    return this.register.get(name);
+  }
 
   registration(data: any) {
     this.userService.registration(data).subscribe();
